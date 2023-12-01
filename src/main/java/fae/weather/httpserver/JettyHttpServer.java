551cf2cfc61d4da11126fc0c6608e7ba52b03ec4
Daniel Junior <jrzabott@gmail.com>
2023-12-01 23:07:05 +0000

    JettyHttpServer#start minimum code to run Jetty and listen to my desired port and send a HTTP/200 response  - jetty version updated.  -

--- src/main/java/fae/weather/httpserver/JettyHttpServer.java
+++ src/main/java/fae/weather/httpserver/JettyHttpServer.java
@@ -1,12 +1,20 @@
 package fae.weather.httpserver;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.eclipse.jetty.server.*;
+import org.eclipse.jetty.util.Callback;
+import org.eclipse.jetty.util.thread.QueuedThreadPool;
+
 /**
  * A implementation of HttpServer using Jetty.
  */
 public class JettyHttpServer implements HttpServer {
 
     private static final String NOT_IMPLEMENTED_YET = "Not implemented yet.";
+    private static final Logger LOGGER = LogManager.getLogger(JettyHttpServer.class);
     private boolean isRunning;
+    private Server server;
 
     public boolean isRunning() {
         return isRunning;
@@ -14,8 +22,42 @@ public class JettyHttpServer implements HttpServer {
 
     @Override
     public void start() {
+
+        // Create and configure a ThreadPool.
+        QueuedThreadPool threadPool = new QueuedThreadPool();
+        threadPool.setName("server");
+
+        // Create a Server instance.
+        server = new Server(threadPool);
+
+        // Create a ServerConnector to accept connections from clients.
+        Connector connector = new ServerConnector(server);
+        ((ServerConnector) connector).setReusePort(true);
+        ((ServerConnector) connector).setPort(10001);
+
+        // Add the Connector to the Server
+        server.addConnector(connector);
+
+        // Set a simple Handler to handle requests/responses.
+        server.setHandler(new Handler.Abstract() {
+            @Override
+            public boolean handle(Request request, Response response, Callback callback) {
+                // Succeed the callback to signal that the
+                // request/response processing is complete.
+                callback.succeeded();
+                return true;
+            }
+        });
+
+        // Start the Server to start accepting connections from clients.
+        try {
+            server.start();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
         // at the end isRunning should be true
-        isRunning = true;
+        isRunning = server.isStarted();
     }
 
     @Override
--- src/test/java/fae/weather/httpserver/JettyHttpServerTest.java
+++ src/test/java/fae/weather/httpserver/JettyHttpServerTest.java
@@ -2,10 +2,18 @@ package fae.weather.httpserver;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URI;
+
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class JettyHttpServerTest {
+
+    private static final String LOCALHOST = "http://localhost";
+    private static final String PORT_DELIM = ":";
+    private final static String HTTP_PORT = "10001";
 
     @Test
     void startJettyHttpServer() {
@@ -20,5 +28,47 @@ class JettyHttpServerTest {
         jettyHttpServer.start();
         jettyHttpServer.stop();
         assertFalse(jettyHttpServer.isRunning());
+    }
+
+    @Test
+    void startAndStopJettyHttpServer() {
+        JettyHttpServer jettyHttpServer = new JettyHttpServer();
+        jettyHttpServer.start();
+        assertTrue(jettyHttpServer.isRunning());
+        jettyHttpServer.stop();
+        assertFalse(jettyHttpServer.isRunning());
+    }
+
+    @Test
+    void startAndStopJettyHttpServerTwice() {
+        JettyHttpServer jettyHttpServer = new JettyHttpServer();
+        jettyHttpServer.start();
+        assertTrue(jettyHttpServer.isRunning());
+        jettyHttpServer.stop();
+        assertFalse(jettyHttpServer.isRunning());
+        jettyHttpServer.start();
+        assertTrue(jettyHttpServer.isRunning());
+        jettyHttpServer.stop();
+        assertFalse(jettyHttpServer.isRunning());
+    }
+
+    @Test
+    void startHttpServerWillStartListeningInPort() throws IOException {
+        JettyHttpServer jettyHttpServer = new JettyHttpServer();
+        jettyHttpServer.start();
+        assertTrue(jettyHttpServer.isRunning());
+
+        //check and assert that port 8080 is listening using java network methods
+        final URI uri = URI.create(LOCALHOST + PORT_DELIM + HTTP_PORT);
+        HttpURLConnection connection = (HttpURLConnection) uri.toURL().openConnection();
+        connection.setRequestMethod("GET");
+
+        // Check that the response code indicates success (e.g., 200 OK; 204 No Content)
+        int responseCode = connection.getResponseCode();
+        assertTrue(responseCode == 200 || responseCode == 204);
+
+        // Stop the server after the test
+        jettyHttpServer.stop();
+
     }
 }
